bbc86715bd4269a2ff319c00e03be09f05b88409
Shahidull Al Anik <anik.kafi404@gmail.com>
2021-10-16 18:08:12 +0600

    user signup adn password hashing added

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Report } from 'src/common/entities/reports/report.entity';
 import { User } from '../../common/entities/users/user.entity';
-import { AuthService } from './auth.service';
-import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
+import { AuthService } from './service/auth.service';
+import { UsersController } from './controller/users.controller';
+import { UsersService } from './service/users.service';
+import { RequestService } from 'src/common/services/request.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, Report])],
   controllers: [UsersController],
-  providers: [UsersService, AuthService]
+  providers: [UsersService, AuthService, RequestService]
 })
 export class UsersModule { }
