b66be6e132a67a7c36a32aca529f086d77377e42
Shahidull Al Anik <anik.kafi404@gmail.com>
2021-10-17 18:20:19 +0600

    user signout & store id into cookie-session added

--- src/modules/users/controller/users.controller.ts
+++ src/modules/users/controller/users.controller.ts
@@ -9,6 +9,7 @@ import {
   Query,
   NotFoundException,
   HttpStatus,
+  Session,
 } from '@nestjs/common';
 import { ApiBody, ApiResponse } from '@nestjs/swagger';
 import { CreateUserDto } from 'src/common/dto/users/create-user.dto';
@@ -20,6 +21,8 @@ import { AuthService } from '../service/auth.service';
 import { UsersService } from '../service/users.service';
 
 // @UseInterceptors(ClassSerializerInterceptor)
+// Controller bounded interceptor
+// Expose only public data while response
 @Serialize(UserDto)
 @Controller('auth')
 export class UsersController {
@@ -28,21 +31,42 @@ export class UsersController {
     private authService: AuthService,
   ) {}
 
+
+  // Show the currently logedIn user
+  @Get('whoami')
+  whoAmI(@Session() session: any) {
+    return this.usersService.findOne(session.userId);
+  }
+
+  // Signout current user
+  @Post('/signout')
+  signOut(@Session() session: any) {
+    session.userId = null;
+  }
+
+  // Create / Signup a new user
   @ApiResponse({ description: 'Create a new user', status: HttpStatus.CREATED })
   @ApiBody({ type: CreateUserDto })
   @Post('/signup')
-  createUser(@Body() createUserDto: CreateUserDto) {
-    const user = this.authService.signup(createUserDto);
+  async createUser(
+    @Body() createUserDto: CreateUserDto,
+    @Session() session: any,) {
+    const user = await this.authService.signup(createUserDto);
+    session.userId = user.id;
     return user;
   }
 
+  // Signin user
   @ApiResponse({ description: 'User signin', status: HttpStatus.OK })
   @Post('/signin')
   @ApiBody({ type: SignInUserDto })
-  signin(@Body() body: SignInUserDto) {
-    return this.authService.signin(body);
+  async signin(@Body() body: SignInUserDto, @Session() session: any) {
+    const user = await this.authService.signin(body);
+    session.userId = user.id;
+    return user;
   }
 
+  // Get an user by ID
   @ApiResponse({ description: 'Get user by ID', status: HttpStatus.OK })
   @Get('/:id')
   async findUser(@Param('id') id: string) {
@@ -55,24 +79,21 @@ export class UsersController {
     return user;
   }
 
-  // @ApiResponse({ description: 'Get all the user by email', status: HttpStatus.OK })
-  // @Get()
-  // findAllUsers(@Query('email') email: string) {
-  //   return this.usersService.find(email);
-  // }
-
+  // Get the list of users
   @ApiResponse({ description: 'Get all the user', status: HttpStatus.OK })
   @Get()
   findAllUsers() {
     return this.usersService.find();
   }
 
+  // Delete an user by ID
   @ApiResponse({ description: 'Delete an user by id', status: HttpStatus.OK })
   @Delete('/:id')
   removeUser(@Param('id') id: string) {
     return this.usersService.remove(id);
   }
 
+  // Update an user by ID
   @ApiResponse({
     description: 'Update an existing user',
     status: HttpStatus.OK,
@@ -82,4 +103,22 @@ export class UsersController {
   updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
     return this.usersService.update(id, body);
   }
+
+  // @ApiResponse({ description: 'Get all the user by email', status: HttpStatus.OK })
+  // @Get()
+  // findAllUsers(@Query('email') email: string) {
+  //   return this.usersService.find(email);
+  // }
+
+  // @ApiResponse({ description: 'Set color session', status: HttpStatus.OK })
+  // @Get('/colors/:color')
+  // setColor(@Param('color') color: string, @Session() session: any) {
+  //   session.color = color;
+  // }
+
+  // @ApiResponse({ description: 'Get color session', status: HttpStatus.OK })
+  // @Get('/colors')
+  // getColor(@Session() session: any) {
+  //   return session.color;
+  // }
 }
